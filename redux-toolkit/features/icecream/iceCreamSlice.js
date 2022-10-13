4dafb065feeda579628af4520a662777b2528e2c
Surya Bhusal <surya@fetch.ly>
2022-10-13 17:04:46 +0545

    Added extra reducer to handle business case for ''when cake is bought, ice cream free.

--- redux-toolkit/features/icecream/iceCreamSlice.js
+++ redux-toolkit/features/icecream/iceCreamSlice.js
@@ -1,8 +1,15 @@
+const { cakeActions } = require('../cake/cakeSlics')
+
 const createSlice = require('@reduxjs/toolkit').createSlice
 
 const initialState = {
     numOfIceCreams: 10
 }
+
+/**
+Extra Reducers for IceCream
+When you order a cake, ice cream is free.
+ */
 
 const iceCreamSlice = createSlice({
     name: 'iceCream',
@@ -14,6 +21,18 @@ const iceCreamSlice = createSlice({
         restocked: (state, action) => {
             state.numOfIceCreams += action.payload
         }
+    },
+    // wenever cake is ordered, we also decreament the no of Icecreams
+    // greatly reduces the complexity    
+    // extraReducers: {
+    //     ['cake/ordered']: (state) => {
+    //         state.numOfIceCreams--
+    //     }
+    // }
+    extraReducers: (builder) => { // this is same as above
+        builder.addCase(cakeActions.ordered, state => {
+            state.numOfIceCreams--
+        })
     }
 })
 
--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -4,9 +4,9 @@ const iceCreamActions = require('./features/icecream/iceCreamSlice').iceCreamAct
 
 console.log('Initial State', store.getState())
 
-// const unsubscribe = store.subscribe(() => {
-//     console.log("Updated state ",store.getState())
-// })
+const unsubscribe = store.subscribe(() => {
+    console.log("Updated state ",store.getState())
+})
 
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
@@ -17,39 +17,46 @@ store.dispatch(iceCreamActions.ordered())
 store.dispatch(iceCreamActions.ordered())
 store.dispatch(iceCreamActions.restocked(2))
 
-// unsubscribe()
+unsubscribe()
 
 /**
 
 Logger COnsole Output
 
 Initial State { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 10 } }
- action cake/ordered @ 16:38:59.793
+Updated state  { cake: { numOfCakes: 9 }, iceCream: { numOfIceCreams: 9 } }
+ action cake/ordered @ 16:58:15.456
    prev state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 10 } }
    action     { type: 'cake/ordered', payload: undefined }
-   next state { cake: { numOfCakes: 9 }, iceCream: { numOfIceCreams: 10 } }
- action cake/ordered @ 16:38:59.794
-   prev state { cake: { numOfCakes: 9 }, iceCream: { numOfIceCreams: 10 } }
+   next state { cake: { numOfCakes: 9 }, iceCream: { numOfIceCreams: 9 } }
+Updated state  { cake: { numOfCakes: 8 }, iceCream: { numOfIceCreams: 8 } }
+ action cake/ordered @ 16:58:15.457
+   prev state { cake: { numOfCakes: 9 }, iceCream: { numOfIceCreams: 9 } }
    action     { type: 'cake/ordered', payload: undefined }
-   next state { cake: { numOfCakes: 8 }, iceCream: { numOfIceCreams: 10 } }
- action cake/ordered @ 16:38:59.795
-   prev state { cake: { numOfCakes: 8 }, iceCream: { numOfIceCreams: 10 } }
+   next state { cake: { numOfCakes: 8 }, iceCream: { numOfIceCreams: 8 } }
+Updated state  { cake: { numOfCakes: 7 }, iceCream: { numOfIceCreams: 7 } }
+ action cake/ordered @ 16:58:15.458
+   prev state { cake: { numOfCakes: 8 }, iceCream: { numOfIceCreams: 8 } }
    action     { type: 'cake/ordered', payload: undefined }
-   next state { cake: { numOfCakes: 7 }, iceCream: { numOfIceCreams: 10 } }
- action cake/restocked @ 16:38:59.795
-   prev state { cake: { numOfCakes: 7 }, iceCream: { numOfIceCreams: 10 } }
+   next state { cake: { numOfCakes: 7 }, iceCream: { numOfIceCreams: 7 } }
+Updated state  { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 7 } }
+ action cake/restocked @ 16:58:15.459
+   prev state { cake: { numOfCakes: 7 }, iceCream: { numOfIceCreams: 7 } }
    action     { type: 'cake/restocked', payload: 3 }
-   next state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 10 } }
- action iceCream/ordered @ 16:38:59.796
-   prev state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 10 } }
+   next state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 7 } }
+Updated state  { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 6 } }
+ action iceCream/ordered @ 16:58:15.459
+   prev state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 7 } }
    action     { type: 'iceCream/ordered', payload: undefined }
-   next state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 9 } }
- action iceCream/ordered @ 16:38:59.796
-   prev state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 9 } }
+   next state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 6 } }
+Updated state  { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 5 } }
+ action iceCream/ordered @ 16:58:15.460
+   prev state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 6 } }
    action     { type: 'iceCream/ordered', payload: undefined }
-   next state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 8 } }
- action iceCream/restocked @ 16:38:59.797
-   prev state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 8 } }
+   next state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 5 } }
+Updated state  { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 7 } }
+ action iceCream/restocked @ 16:58:15.461
+   prev state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 5 } }
    action     { type: 'iceCream/restocked', payload: 2 }
-   next state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 10 } }
+   next state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 7 } }
  */
