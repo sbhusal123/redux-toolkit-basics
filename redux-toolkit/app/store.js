c7f69c7ed9412ff91f1d1d7ab5d227e29af4f092
Surya Bhusal <surya@fetch.ly>
2022-10-13 16:35:03 +0545

    Created a iceCream reducers and slices.

--- redux-toolkit/app/store.js
+++ redux-toolkit/app/store.js
@@ -1,10 +1,12 @@
 const configureSore = require('@reduxjs/toolkit').configureStore
 
 const cakeReducer = require('../features/cake/cakeSlics')
+const iceCreamReducer = require('../features/icecream/iceCreamSlice')
 
 const store = configureSore({
     reducer: {
-        cake: cakeReducer
+        cake: cakeReducer,
+        iceCream: iceCreamReducer
     }
 })
 module.exports = store
--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -1,5 +1,6 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlics').cakeActions
+const iceCreamActions = require('./features/icecream/iceCreamSlice').iceCreamActions
 
 console.log('Initial State', store.getState())
 
@@ -11,5 +12,9 @@ store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.restocked(3))
+
+store.dispatch(iceCreamActions.ordered())
+store.dispatch(iceCreamActions.ordered())
+store.dispatch(iceCreamActions.restocked(2))
 
 unsubscribe()
