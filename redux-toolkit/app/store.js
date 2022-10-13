fad8037de0a929858f9f13d61cc759fc7b508e0c
Surya Bhusal <surya@fetch.ly>
2022-10-13 16:40:20 +0545

    Added redux logger middleware the store.

--- redux-toolkit/app/store.js
+++ redux-toolkit/app/store.js
@@ -3,10 +3,15 @@ const configureSore = require('@reduxjs/toolkit').configureStore
 const cakeReducer = require('../features/cake/cakeSlics')
 const iceCreamReducer = require('../features/icecream/iceCreamSlice')
 
+// logger setup
+const reduxLogger = require('redux-logger')
+const logger = reduxLogger.createLogger()
+
 const store = configureSore({
     reducer: {
         cake: cakeReducer,
         iceCream: iceCreamReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 module.exports = store
--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -4,9 +4,9 @@ const iceCreamActions = require('./features/icecream/iceCreamSlice').iceCreamAct
 
 console.log('Initial State', store.getState())
 
-const unsubscribe = store.subscribe(() => {
-    console.log("Updated state ",store.getState())
-})
+// const unsubscribe = store.subscribe(() => {
+//     console.log("Updated state ",store.getState())
+// })
 
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
@@ -17,4 +17,39 @@ store.dispatch(iceCreamActions.ordered())
 store.dispatch(iceCreamActions.ordered())
 store.dispatch(iceCreamActions.restocked(2))
 
-unsubscribe()
+// unsubscribe()
+
+/**
+
+Logger COnsole Output
+
+Initial State { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 10 } }
+ action cake/ordered @ 16:38:59.793
+   prev state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 10 } }
+   action     { type: 'cake/ordered', payload: undefined }
+   next state { cake: { numOfCakes: 9 }, iceCream: { numOfIceCreams: 10 } }
+ action cake/ordered @ 16:38:59.794
+   prev state { cake: { numOfCakes: 9 }, iceCream: { numOfIceCreams: 10 } }
+   action     { type: 'cake/ordered', payload: undefined }
+   next state { cake: { numOfCakes: 8 }, iceCream: { numOfIceCreams: 10 } }
+ action cake/ordered @ 16:38:59.795
+   prev state { cake: { numOfCakes: 8 }, iceCream: { numOfIceCreams: 10 } }
+   action     { type: 'cake/ordered', payload: undefined }
+   next state { cake: { numOfCakes: 7 }, iceCream: { numOfIceCreams: 10 } }
+ action cake/restocked @ 16:38:59.795
+   prev state { cake: { numOfCakes: 7 }, iceCream: { numOfIceCreams: 10 } }
+   action     { type: 'cake/restocked', payload: 3 }
+   next state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 10 } }
+ action iceCream/ordered @ 16:38:59.796
+   prev state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 10 } }
+   action     { type: 'iceCream/ordered', payload: undefined }
+   next state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 9 } }
+ action iceCream/ordered @ 16:38:59.796
+   prev state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 9 } }
+   action     { type: 'iceCream/ordered', payload: undefined }
+   next state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 8 } }
+ action iceCream/restocked @ 16:38:59.797
+   prev state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 8 } }
+   action     { type: 'iceCream/restocked', payload: 2 }
+   next state { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 10 } }
+ */
