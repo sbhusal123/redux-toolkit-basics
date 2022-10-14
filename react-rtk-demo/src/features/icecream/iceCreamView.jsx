431bd35b5e510739b94ddb188ad36d4cf49322ef
Surya Bhusal <surya@fetch.ly>
2022-10-14 13:04:10 +0545

    Created views for cake, ice creams and users.

---
+++ react-rtk-demo/src/features/icecream/iceCreamView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const IceCreamView = () => {
+  return (
+    <div>
+        <h2>Number of ice creams - </h2>
+        <button>Order ice creams</button>
+        <button>Restock ice creams</button>
+    </div>
+  )
+}
