eddb9ec65637f3bea797d306d3153cfdc1dc0732
Surya Bhusal <surya@fetch.ly>
2022-10-14 13:07:24 +0545

    Connected react app with the redux store.

--- react-rtk-demo/src/App.jsx
+++ react-rtk-demo/src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 import {CakeView} from './features/cake/cakeView';
 import {IceCreamView} from './features/icecream/iceCreamView';
 import {UserView} from './features/user/userView';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
